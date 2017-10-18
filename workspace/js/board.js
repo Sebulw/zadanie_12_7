ea69889fd6d10332a9a56176f9a8bd14cae495e7
Sebastian Wojnicz <sebulw@gmail.com>
2017-10-18 14:06:07 +0200

    ready to check, working on new feature 'ui-sortable for columns'

--- workspace/js/board.js
+++ workspace/js/board.js
@@ -1,20 +1,31 @@
 var board = {
 	name: 'Tablica Kanban',
 	createColumn: function(column) {
-	  this.element.append(column.element);
-	  initSortable();
+	  	this.element.append(column.element);
+	  	initSortable();
 	},
 	element: $('#board .column-container')
 };
 
 $('.create-column')
-	.click(function(){
-		board.createColumn(new Column(prompt('Wpisz nazwę kolumny')));
+	.click(function() {
+		var columnName = prompt('Wpisz nazwę kolumny');
+		$.ajax({
+			url: baseUrl + '/column',
+			method: 'POST',
+			data: {
+				name: columnName
+			},
+			success: function(response) {
+				var column = new Column(response.id, columnName);
+				board.createColumn(column);
+			}
+		});
 	});
 	
 function initSortable() {
     $('.card-list').sortable({
-      connectWith: '.card-list',
-      placeholder: 'card-placeholder'
+      	connectWith: '.card-list',
+      	placeholder: 'card-placeholder'
     }).disableSelection();
-  }
+}
--- workspace/js/column.js
+++ workspace/js/column.js
@@ -1,8 +1,7 @@
-function Column(name) {
-	var self = this;
-	
-	this.id = randomString();
-	this.name = name;
+function Column(id, name) {
+	var self = this;	
+	this.id = id;
+	this.name = name || 'No name given';
 	this.element = createColumn();
 
 	function createColumn() {
@@ -11,7 +10,7 @@ function Column(name) {
 		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
-		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
+		var columnAddCard = $('<button class="column-add-card">+</button>');
 		
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
 		columnDelete.click(function() {
@@ -19,8 +18,20 @@ function Column(name) {
 		});
 		
 		columnAddCard.click(function(event) {
+			var cardName = prompt('Enter the name of the card');
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+				url: baseUrl + '/card',
+				method: 'POST',
+				data: {
+					name: cardName,
+					bootcamp_kanban_column_id: self.id
+				},
+				success: function(response) {
+					var card = new Card(response.id, cardName);
+					self.createCard(card);
+				}
+			});
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
@@ -33,9 +44,16 @@ function Column(name) {
 	}
 Column.prototype = {
 	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+	  	this.element.children('ul').append(card.element);
 	},
 	deleteColumn: function() {
-	  this.element.remove();
+	  	var self = this;
+	  	$.ajax({
+	  		url: baseUrl + '/column/' + self.id,
+	  		method: 'DELETE',
+	  		success: function(response) {
+	  			self.element.remove();
+	  		}
+	  	});
 	}
 };
